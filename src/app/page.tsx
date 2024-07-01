f7ff5e75249f2c4dc394784ee6553fe988510de6
aknirojan <143987182+aknirojan@users.noreply.github.com>
2024-07-01 12:58:05 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
 'use client';
 import React from 'react';
-import '@/styles/home.css';
-import Parallax from '@/templates/Home/Parralax';
-import Services from '@/templates/Home/Service';
-import Industries from '@/templates/Home/Industries';
 
 function Page() {
   return (
-    <main className='header-container'>
-      <Parallax />
-      <div className='home-bottom-background'>
+    <main>
+      <div>
         <div className='uppercase text-white poppins-bold text-4xl text-center py-40'>
           Our Services
-        </div>
-
-        <div>
-          <Services />
-        </div>
-
-        <div className='py-40'>
-          <Industries />
         </div>
       </div>
     </main>
